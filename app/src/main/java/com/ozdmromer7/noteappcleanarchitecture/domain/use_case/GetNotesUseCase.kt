163baa62769a2ae092702f43abacf234b073e317
ozdmromer7 <omer070698@hotmail.com>
2023-11-27 18:58:41 +0300

    getNotesUseCase has been completed.

---
+++ app/src/main/java/com/ozdmromer7/noteappcleanarchitecture/domain/use_case/GetNotesUseCase.kt
@@ -0,0 +1,36 @@
+package com.ozdmromer7.noteappcleanarchitecture.domain.use_case
+
+import com.ozdmromer7.noteappcleanarchitecture.domain.model.Notes
+import com.ozdmromer7.noteappcleanarchitecture.domain.repository.NoteRepository
+import com.ozdmromer7.noteappcleanarchitecture.domain.util.NoteOrder
+import com.ozdmromer7.noteappcleanarchitecture.domain.util.OrderType
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class GetNotesUseCase(private val repository: NoteRepository) {
+
+    operator fun invoke(noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending)): Flow<List<Notes>> {
+
+        return repository.getNotes().map { notes ->
+            when (noteOrder.orderType) {
+                is OrderType.Ascending -> {
+                    when (noteOrder) {
+                        is NoteOrder.Title -> notes.sortedBy { it.title.lowercase() }
+                        is NoteOrder.Color -> notes.sortedBy { it.color }
+                        is NoteOrder.Date -> notes.sortedBy { it.timeStamp }
+                    }
+                }
+
+                is OrderType.Descending -> {
+                    when (noteOrder) {
+                        is NoteOrder.Title -> notes.sortedByDescending { it.title.lowercase() }
+                        is NoteOrder.Color -> notes.sortedByDescending { it.color }
+                        is NoteOrder.Date -> notes.sortedByDescending { it.timeStamp }
+                    }
+                }
+            }
+        }
+
+
+    }
+}
